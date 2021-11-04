fbf5bb297f5012adff10f27707b67eb01cd69a2c
SpicyRicecaker <awesomenessisgolden@gmail.com>
2021-11-03 23:22:50 -0700

    SO COOL, def sine wave works hehe

--- program/src/main.rs
+++ program/src/main.rs
@@ -1,3 +1,18 @@
+use rodio::{source::SineWave, OutputStream, Sink, Source};
+use std::{
+    thread,
+    time::{self, Duration},
+};
+
+/// Checkout https://docs.rs/rodio/0.14.0/rodio/
 fn main() {
-    println!("Hello, world!");
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+
+    let sink_left = Sink::try_new(&stream_handle).unwrap();
+    // let sink_right = Sink::try_new(&stream_handle).unwrap();
+
+    let source = SineWave::new(250).take_duration(Duration::from_secs(30));
+    sink_left.append(source);
+
+    sink_left.sleep_until_end();
 }
